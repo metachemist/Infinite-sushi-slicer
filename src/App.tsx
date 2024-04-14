2d8ff227701015f3602f361547e018958e4d69d3
metachemist <meta.chemist.22@gmail.com>
2024-04-14 23:41:24 +0500

    Adding config files

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,35 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { useState } from "react";
-import styles from "./App.module.css";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-export default function App() {
-  const [slices, setSlices] = useState(
-    Array(6).fill({ id: Math.random(), state: "" })
-  );
-
-  const [_animating, setAnimating] = useState(false);
-
-  const handleSliceClick = (index: number) => {
-    setAnimating(true);
-
-    const updatedSlices = slices.map((slice, i) =>
-      i === index ? { ...slice, state: "removing" } : slice
-    );
-    setSlices(updatedSlices);
-
-    setTimeout(() => {
-      setSlices((current) => current.filter((_, i) => i !== index));
-      addSlice();
-    }, 500);
-  };
-
-  const addSlice = () => {
-    setSlices((currentSlices) => [
-      ...currentSlices,
-      { id: Math.random(), state: "adding" },
-    ]);
-
-    setTimeout(() => {
-      setSlices((currentSlices) =>
-        currentSlices.map((slice) =>
-          slice.state === "adding" ? { ...slice, state: "" } : slice
-        )
-      );
-      setAnimating(false);
-    }, 500);
-  };
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <div className={styles.roll}>
-      {slices.map((slice, index) => (
-        <div
-          key={slice.id}
-          className={`${styles.slice} ${styles[slice.state]}`}
-          onClick={() => handleSliceClick(index)}
-        ></div>
-      ))}
-    </div>
-  );
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
+
+export default App
